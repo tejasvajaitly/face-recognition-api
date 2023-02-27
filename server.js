3b5a37f5ff1d39d27308a020038a98a62e48e1d6
Tejasva Jaitly <tejasvajaitly@gmail.com>
2023-02-26 18:48:27 -0800

    changed db env var

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    port: 5432,
-    user: "neiljaitly",
-    password: "",
-    database: "face_recognition",
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
 });
 
